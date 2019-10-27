22624891f10e7dd9bfc6b0b7d954a341b7ae2a60
K-Real28 <55459077+K-Real28@users.noreply.github.com>
2019-10-27 13:12:01 +0300

    Update source.c

--- LabPr/source.c
+++ LabPr/source.c
@@ -1,24 +1,27 @@
-#define _USE_Defines
-#include<math.h>
-#include <locale.h>
-int main() {
-	int x1, y1, x2, y2, x3, y3;
-	int P, p, S, A, B, C;
-	setlocale(LC_ALL, "RUS");
-	printf("Введите первую координату");
-	scanf_s("%d,%d",&x1,&y1);
-	printf("Введите вторую координату");
-	scanf_s("%d,%d",&x2,&y2);
-	printf("Введите третью координату");
-	scanf_s("%d,%d", &x3, &y3);
-	A = sqrt(pow(x2 - x1) + pow(y2 - y1));
-	B = sqrt(pow(x3 - x2) + pow(y3 - y2));
-	C = sqrt(pow(x1 - x3) + pow(y1 - y3));
-	P = A + B + C;
-	p = P / 2;
-	S = sqrt(p(p - A) * (p - B) * (p - C));
-	printf("Пиримитр равен: %d", P);
-	printf("Площадь равна:%d", S);
-    _getch();
-   return 0;
+#include<stdio.h>
+#include<stdlib.h>
+#define n 10
+#include<conio.h>
+#include<locale.h>
+void main(void)
+{
+	int i, g, l;
+	double j = 0, k = 0, t = 0;
+	int A[] = { 0, 0, 10, 20, 10, 10, 20, 10, 0, 10, 10, 20, 10 ,10,20,10,20,30,20,10,50,0,10,10,0,10,0,20,10,10 };
+	setlocale(LC_ALL, "Rus");
+	printf("Cведения о количестве осадков:");
+	for (i = 0; i < 10; i++) {
+		j += A[i];
+		printf("\t%d", A[i]);
+	}
+	for (g = 10;g < 20; g++) {
+		k += A[g];
+		printf("\t%d", A[g]);
+	}
+	for (l = 20; l < 30; l++) {
+		t += A[l];
+		printf("\t%d", A[l]);
+	}
+	printf("\nВ среднем за один день в первую декаду месяца выпало:%.1lf осадков.\nВо второую декаду месяца :%.1lf осадков.\nВ третью декаду месяца:%.1lf осадков",j/10,k/10,t/10);
+	_getch();
 }
